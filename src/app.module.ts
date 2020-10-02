8e4e9e82c701c7850f1177c96e6d985d496d8b3a
Eduards Sniedze <eduards.sniedze@gmail.com>
2020-10-02 16:45:31 +0300

    Add categories and products

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProfilesModule } from './profiles/profiles.module';
+import { ProductsModule } from './products/products.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [UsersModule, ProfilesModule],
+  imports: [UsersModule, ProfilesModule, ProductsModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
